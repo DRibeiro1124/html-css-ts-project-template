95c80d7b0379d87210b250e94ad2659dd2d8f717
Gavin Stark <gavin@gstark.com>
2021-07-18 09:00:16 -0400

    Places vite.config.ts in the correct directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
 import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), checker({ typescript: true })],
+  plugins: [checker({ typescript: true })],
 })
